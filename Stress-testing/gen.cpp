9e656689434e4b7f7a06436b749a966617fc2a60
sourav739397 <sourav739397@gmail.com>
2025-01-25 18:40:13 +0600

    add stress testing

---
+++ Stress-testing/gen.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int32_t main() {
+    
+    return 0;
+}
