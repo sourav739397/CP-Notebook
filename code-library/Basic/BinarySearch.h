2665220dd87db0362c468d4af256db33c4cd533d
Sourav Mondal <sourav739397@gmail.com>
2025-02-14 15:26:54 +0600

    add code-library

---
+++ code-library/Basic/BinarySearch.h
@@ -0,0 +1,11 @@
+template <typename T, typename U>
+T lstTrue(T lo, T hi, U f) {
+    --lo;
+    assert(lo <= hi);
+    while (lo < hi) {
+        T mid = lo + (hi - lo + 1) / 2;
+        f(mid) ? lo = mid : hi = mid - 1;
+    }
+    return lo;
+}
+
