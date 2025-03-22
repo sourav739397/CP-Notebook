3d669e6642b8353e2776281d18f2027c8b06f912
sourav739397 <sourav739397@gmail.com>
2025-03-23 01:27:25 +0600

    add sol

---
+++ sol.cpp
@@ -0,0 +1,23 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define dbg(...)
+#endif
+
+using i64 = long long;
+
+void solve() {
+    
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(nullptr); 
+
+    int Q; 
+    cin >> Q; 
+    while(Q--) solve(); 
+}
