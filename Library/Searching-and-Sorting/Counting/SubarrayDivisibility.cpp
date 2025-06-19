904ae077fdccbbde1088fd1ad0ea7393137ec2f9
sourav739397 <sourav739397@gmail.com>
2025-06-19 09:13:59 +0600

    use vector instead of gp_hash_table

--- Library/Searching-and-Sorting/Counting/SubarrayDivisibility.cpp
+++ Library/Searching-and-Sorting/Counting/SubarrayDivisibility.cpp
@@ -1,13 +1,11 @@
 /*
- *  Description: Count number of subarrays sum divisible by x
+ *  Description: Count number of subarrays whose sum is divisible by x
  *  Verification: https://cses.fi/problemset/task/1662
  *  Time Complexity: O(n)
 */
 
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-using namespace __gnu_pbds;
 
 #ifdef LOCAL
 #include "debug.h"
@@ -20,9 +18,9 @@ int32_t main() {
     
     int n;    cin >> n;
 
-    gp_hash_table<int, int> F;
-    int64_t sum = 0, ans = 0; F[0] = 1;
-    for (int i = 0; i < n; ++i) {
+    int64_t ans = 0; 
+    vector<int> F(n); F[0] = 1; // use gp_hash_table value of x is >= 10^6
+    for (int i = 0, sum = 0; i < n; ++i) {
         int x; cin >> x;
         sum += x; sum = ((sum%n) + n) % n;
         ans += F[sum];
