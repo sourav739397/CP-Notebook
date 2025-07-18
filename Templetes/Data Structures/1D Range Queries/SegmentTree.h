e9ba56adfec8c672ea6efd7f20473bae1e7fa66d
Sourav Mondal <sourav739397@gmail.com>
2025-07-18 13:34:19 +0600

    Create SegmentTree.h

---
+++ Templetes/Data Structures/1D Range Queries/SegmentTree.h
@@ -0,0 +1,22 @@
+template<typename T> struct SegTree {
+    const T ID{};
+    T cmb(T a, T b) { return a + b; }
+    int n = 1; vector<T> seg;
+    SegTree(int SZ) {
+        while (n < SZ) n *= 2;
+        seg.assign(2*n, ID);
+   }
+    void pull(int p) { seg[p] = cmb(seg[2*p], seg[2*p+1]); }
+    void update(int p, T val) { // set value at position p
+        seg[p += n] = val;
+        for (p /= 2; p; p /= 2) pull(p);
+    }
+    T query(int l, int r) { // zero-indexed, inclusive
+        T ra = ID, rb = ID;
+        for (l += n, r += n + 1; l < r; l /= 2, r /= 2) {
+            if (l&1) ra = cmb(ra, seg[l++]);
+            if (r&1) rb = cmb(seg[--r], rb);
+        }
+        return cmb(ra, rb);
+    }
+};
