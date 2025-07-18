768484169e854992eb1e2791673457ad3987ba8a
Sourav Mondal <sourav739397@gmail.com>
2025-07-18 14:21:13 +0600

    pre -> sum ; grid -> v;

--- Templetes/Data Structures/Static Range Queries/PrefixSum2D.h
+++ Templetes/Data Structures/Static Range Queries/PrefixSum2D.h
@@ -1,16 +1,17 @@
 template<typename T> struct PrefixSum2D {
-    vector<vector<T>> pre;
-    PrefixSum2D (const auto& grid) {
-        int n = grid.size()+1, m = grid[0].size()+1;
-        pre.assign(n, vector<T>(m, T{}));
-        for (int i = 1; i < n; ++i) {
-            for (int j = 1; j < m; ++j) {
-                pre[i][j] = grid[i-1][j-1] + pre[i-1][j] + pre[i][j-1] - pre[i-1][j-1];
+    vector<vector<T>> sum;
+    PrefixSum2D (const vector<vector<T>>& v) {
+        int n = v.size(), m = v[0].size();
+        sum.assign(n+1, vector<T>(m+1, T{}));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                sum[i+1][j+1] = v[i][j]-sum[i][j]
+                +sum[i+1][j]+sum[i][j+1];
             }
         }
     }
-
-    T query (int r1, int c1, int r2, int c2) {
-        return pre[r2][c2] - pre[r1-1][c2] - pre[r2][c1-1] + pre[r1-1][c1-1];
+    T query (int x1, int y1, int x2, int y2) {
+        return sum[x2][y2]+sum[x1-1][y1-1]
+        -sum[x1-1][y2]-sum[x2][y1-1];
     }
 };
