3360f0311460a0f6534f8a9367c2f5a014ac30d9
Sourav Mondal <sourav739397@gmail.com>
2025-07-18 13:43:06 +0600

    value of n remain unchange in templete PrefixSum.h

--- Templetes/Data Structures/1D Range Queries/PrefixSum.h
+++ Templetes/Data Structures/1D Range Queries/PrefixSum.h
@@ -1,14 +1,12 @@
 template<typename T> struct PrefixSum {
     vector<T> pre;
     PrefixSum (const auto& arr) {
-        int n = arr.size()+1;
-        pre.assign(n, T{});
-        for (int i = 1; i < n; ++i) {
-            pre[i] = a[i-1] + pre[i-1];
+        int n = arr.size();
+        pre.assign(n+1, T{});
+        for (int i = 0; i < n; ++i) {
+            pre[i+1] = a[i] + pre[i];
         }
     }
 
-    T query (int l, int r) {
-        return pre[r] - pre[l-1];
-    }
+    T query (int l, int r) { return pre[r] - pre[l-1]; }
 };
