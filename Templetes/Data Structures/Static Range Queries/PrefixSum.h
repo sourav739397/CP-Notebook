446f35544cadf1fc421871b05453cd980c7dd7cc
Sourav Mondal <sourav739397@gmail.com>
2025-07-18 13:58:06 +0600

    Create PrefixSum.h

---
+++ Templetes/Data Structures/Static Range Queries/PrefixSum.h
@@ -0,0 +1,11 @@
+template<typename T> struct PrefixSum {
+    vector<T> pre;
+    PrefixSum (const auto& arr) {
+        int n = arr.size();
+        pre.assign(n+1, T{});
+        for (int i = 0; i < n; ++i) {
+            pre[i+1] = arr[i] + pre[i];
+        }
+    }
+    T query (int l, int r) { return pre[r] - pre[l-1]; }
+};
